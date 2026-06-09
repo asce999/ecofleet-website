b10352ffb169cfd209ed0c3fa45d8f4e5093ebde
Aryan <ardcruz.20037@gmail.com>
2026-06-09 11:08:41 +0530

    Custom 404 page, various small changes

--- ecofleet/settings.py
+++ ecofleet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3g*br9bf=0dn)$22w6jkzfdjt%*s395pm81a_+4xbgv6%nex_!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
--- ecofleet/urls.py
+++ ecofleet/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+def custom_404(request, exception):
+    return render(request, 'core/404.html', status=404)
+
+handler404 = custom_404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
